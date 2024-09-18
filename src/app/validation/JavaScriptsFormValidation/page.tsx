c5cfc261f0bba110ca30d2e03d560a3ee5b13645
mohdbinharun56 <arahaman461@gmail.com>
2024-09-18 22:46:40 +0600

    complete validation with react

--- src/app/validation/JavaScriptsFormValidation/page.tsx
+++ src/app/validation/JavaScriptsFormValidation/page.tsx
@@ -1,77 +1,55 @@
-// import { useState } from "react";
+'use client'
+import { useState } from "react";
 
-// export default function Register() {
+document.getElementById("fullname")
+export default function Register() {
 
-//     const [name,setName] = useState('');
-//     const [email,setEmail] = useState('');
-//     const [password,setPassword] = useState('');
-//     const [confirmpassword,setConformPassword] = useState('');
-//     const [dateOfBirth,setDateOfBirth] = useState('');
-//     const [gender,setGender] = useState('');
-//     const [error,setError] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    // const [error,setError] = useState('');
     
-//     const handleRegister = (e:any) => {
-//         e.preventDefault();
-//         // const name = document.getElementById("fullname");
-//         // const email = document.getElementById("email");
-//         // const password = document.getElementById("password");
-//         // const confirmpassword = document.getElementById("confirmpassword");
-//         // const dateOfBirth = document.getElementById("date");
-//         // const gender = document.getElementById("gender");
+    const handleOnEmailChange = (e:any) =>{
+        setEmail(e.target.value);
+    }
 
-//         if(!name){
-//             setError('name is required!');
-//         }else if(!email){
-//             setError('email is required!');
-//         }else if(!password){
-//             setError('password is required!');
-//         }else if(!confirmpassword){
-//             setError('confirm password is required!');
-//         }else if(!dateOfBirth){
-//             setError('DateOfBirth is required!');
-//         }else if(!gender){
-//             setError('Gender is required!');
-//         }
+    const handleOnPassworChange = (e:any) =>{
+        setPassword(e.target.value);
+    }
+    const handleRegister = (e:any) => {
+        e.preventDefault();
+        
+        if(!email){
+            // setError('Email is required');
+            alert('Email is required');
+        }else if(!password){
+            // setError('Password is Required');
+            alert('Password is Required');
+        }else{
+            // console.log()
+            setEmail('');
+            setPassword('');
+            alert('Login success');
+            
+        }
+    }
 
-//     }
 
-
-//     return (
-//         <>
-//             <form className="register" onSubmit={handleRegister}>
-//                 <div>
-//                     <span>Full Name</span>
-//                     <input type="text" id="fullname" required></input>
-//                 </div>
-//                 <div>
-//                     <span>Email</span>
-//                     <input type="email" id="email" required></input>
-//                 </div>
-//                 <div>
-//                     <span>Password</span>
-//                     <input type="password" id="password" required></input>
-//                 </div>
-//                 <div>
-//                     <span>Confirm Password</span>
-//                     <input type="password" id="confirmpassword" required></input>
-//                 </div>
-//                 <div>
-//                     <span>Date of Birth</span>
-//                     <input type="date" id="date" required></input>
-//                 </div>
-//                 <div>
-//                     <span>Gender</span>
-//                     <select id="gender">
-//                         <option value="Select">Select</option>
-//                         <option value="Select">Male</option>
-//                         <option value="Select">Female</option>
-//                     </select>
-//                 </div>
-//                 <div>
-//                     {/* <span>Full Name</span> */}
-//                     <input type="submit" id="btnregister" value="Register"></input>
-//                 </div>
-//             </form>
-//         </>
-//     );
-// } 
+    return (
+        <>
+            <form className="register" onSubmit={handleRegister}>
+                <div>
+                    <span>Email</span>
+                    <input type="email" id="email" value={email} onChange={handleOnEmailChange} ></input>
+                    
+                </div>
+                <div>
+                    <span>Password</span>
+                    <input type="password" id="password" value={password} onChange={handleOnPassworChange} minLength={8}></input>
+                </div>
+                <div>
+                    <input type="submit" id="btnregister" value="Register"></input>
+                </div>
+            </form>
+        </>
+    );
+} 
